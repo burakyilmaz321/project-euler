faa83f989656e38f7d73f72678a1a859a64bdd91
Burak Yılmaz <burakyilmaz321@gmail.com>
2017-07-11 23:56:47 +0300

    add memo

--- 14_longest_collatz.py
+++ 14_longest_collatz.py
@@ -1,3 +1,12 @@
+def memo(func):
+    memory = dict()
+    def memoize(arg):
+        if arg not in memory:
+            memory[arg] = func(arg)
+        return memory[arg]
+    return memoize
+
+@memo
 def collatz(n):
     if n == 1:
         return 1
